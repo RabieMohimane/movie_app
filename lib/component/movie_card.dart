413b3a7379d59929a0e7933e0c18c5bd9f9783be
rabie888 <rabie.mohimane@gmail.com>
2025-07-31 12:42:57 +0100

    show images in movie details and hide  read more button in details

--- lib/component/movie_card.dart
+++ lib/component/movie_card.dart
@@ -3,12 +3,10 @@ import 'package:movie_app/models/movie.dart';
 import 'package:movie_app/pages/movie_details.dart';
 
 class MovieCard extends StatelessWidget {
-  const MovieCard({
-    super.key,
-    required this.movie,
-  });
+  const MovieCard({super.key, required this.movie,this.hideButton = false });
 
   final Movie movie;
+  final bool hideButton;
 
   @override
   Widget build(BuildContext context) {
@@ -16,50 +14,68 @@ class MovieCard extends StatelessWidget {
       child: ExpansionTile(
         title: Text(movie.title!),
         subtitle: Text('Director: ${movie.director}'),
-        leading: CircleAvatar(child: Text(movie.title![0])),
-        trailing: Icon(Icons.arrow_drop_down),  
+        leading: CircleAvatar(backgroundImage: NetworkImage(movie.images![0])),
+        trailing: Icon(Icons.arrow_drop_down),
         children: [
-         Padding(
-          padding: const EdgeInsets.only(left:75.0, right: 10.0, bottom: 10.0,top:8.0),
-          child:Column(
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 75.0,
+              right: 10.0,
+              bottom: 10.0,
+              top: 8.0,
+            ),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 RichText(
-                  text: 
-                    TextSpan(
-                      style: DefaultTextStyle.of(context).style,    
-                      children: [
-                     TextSpan(text:'Released: ',
-                      style: (Theme.of(context)
-                      .textTheme.labelLarge ?? const TextStyle())
-                     .copyWith(fontWeight: FontWeight.bold)),
-                       
-                      TextSpan(text: '${movie.year}\n', ),
-                       TextSpan(text:'Genre: ',
-                      style: (Theme.of(context)
-                      .textTheme.labelLarge ?? const TextStyle())
-                     .copyWith(fontWeight: FontWeight.bold)),
-                       
-                      TextSpan(text: '${movie.genre}\n', ),
-                       TextSpan(text:'Plot: ',
-                      style: (Theme.of(context)
-                      .textTheme.labelLarge ?? const TextStyle())
-                     .copyWith(fontWeight: FontWeight.bold)),
-                       
-                      TextSpan(text: '${movie.plot}\n', ),
-                       
-                       
-                
-                      
-              ]),
-    
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: [
+                      TextSpan(
+                        text: 'Released: ',
+                        style:
+                            (Theme.of(context).textTheme.labelLarge ??
+                                    const TextStyle())
+                                .copyWith(fontWeight: FontWeight.bold),
+                      ),
+
+                      TextSpan(text: '${movie.year}\n'),
+                      TextSpan(
+                        text: 'Genre: ',
+                        style:
+                            (Theme.of(context).textTheme.labelLarge ??
+                                    const TextStyle())
+                                .copyWith(fontWeight: FontWeight.bold),
+                      ),
+
+                      TextSpan(text: '${movie.genre}\n'),
+                      TextSpan(
+                        text: 'Plot: ',
+                        style:
+                            (Theme.of(context).textTheme.labelLarge ??
+                                    const TextStyle())
+                                .copyWith(fontWeight: FontWeight.bold),
+                      ),
+
+                      TextSpan(text: '${movie.plot}\n'),
+                    ],
+                  ),
                 ),
-                TextButton(onPressed: ()=>{
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> MovieDetails(movie: movie,)))
-                }, child: Text('Read More')),
-              ] ,
-        )
-      )
+                if (!hideButton)
+                  TextButton(
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetails(movie: movie),
+                        ),
+                      ),
+                    },
+                    child: Text('Read More'),
+                  ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/pages/movie_details.dart
+++ lib/pages/movie_details.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/component/movie_card.dart';
 import 'package:movie_app/models/movie.dart';
 
 class MovieDetails extends StatelessWidget {
   const MovieDetails({super.key, required this.movie});
   final Movie movie;
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text(movie.title ?? 'Movie Details'),
-        actions: const [],),
+        title: Text(movie.title ?? 'Movie Details'),
+        actions: const [],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            MovieCard(movie: movie,hideButton: true),
+            Text(
+              'Movie Posters',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: movie.images?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4.0,
+                    child: Image.network(
+                      movie.images![index],
+                      fit: BoxFit.contain,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
